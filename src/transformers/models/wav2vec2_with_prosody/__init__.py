bff24050975a6dfb79b08f7b90c9e4422f13f79a
Jakkrapun123 <113134065+Jakkrapun123@users.noreply.github.com>
2025-09-06 17:28:52 +0700

    Update __init__.py

--- src/transformers/models/wav2vec2_with_prosody/__init__.py
+++ src/transformers/models/wav2vec2_with_prosody/__init__.py
@@ -18,13 +18,13 @@ from ...utils.import_utils import define_import_structure
 
 
 if TYPE_CHECKING:
-    from .configuration_wav2vec2 import *
-    from .feature_extraction_wav2vec2 import *
-    from .modeling_flax_wav2vec2 import *
-    from .modeling_tf_wav2vec2 import *
-    from .modeling_wav2vec2 import *
-    from .processing_wav2vec2 import *
-    from .tokenization_wav2vec2 import *
+    from .configuration_wav2vec2pro import *
+    from .feature_extraction_wav2vec2pro import *
+    from .modeling_flax_wav2vec2pro import *
+    from .modeling_tf_wav2vec2pro import *
+    from .modeling_wav2vec2pro import *
+    from .processing_wav2vec2pro import *
+    from .tokenization_wav2vec2pro import *
 else:
     import sys
 
